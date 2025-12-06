e15ee80aaa40bd4277f237e0f03bdc5cecb3f7e4
TerrestrialAlien <73656294+TerrestrialAlien@users.noreply.github.com>
2025-12-06 13:06:25 -0500

    Update ClientLoader.client.lua

--- StarterPlayer/StarterPlayerScripts/ClientLoader.client.lua
+++ StarterPlayer/StarterPlayerScripts/ClientLoader.client.lua
@@ -6,19 +6,19 @@ local loadedControllers = {}
 
 -- 1. Load all modules
 for _, module in ipairs(Controllers:GetChildren()) do
-    if module:IsA("ModuleScript") then
-        loadedControllers[module.Name] = require(module)
-    end
+	if module:IsA("ModuleScript") then
+		loadedControllers[module.Name] = require(module)
+	end
 end
 
 -- 2. Initialize them
 for _, controller in pairs(loadedControllers) do
-    if controller.Init then controller:Init(loadedControllers) end
+	if controller.Init then controller:Init(loadedControllers) end
 end
 
 -- 3. Start them
 for _, controller in pairs(loadedControllers) do
-    if controller.Start then controller:Start() end
+	if controller.Start then controller:Start() end
 end
 
 print("ClientLoader finished loading controllers.")
